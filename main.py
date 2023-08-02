20f475f168e5977faca34fb23cea0a08b9b9193d
wizardy <wizardhassan99@gmail.com>
2023-08-02 13:25:36 +0100

    optional parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -7,14 +8,29 @@ app = FastAPI()
 @app.get('/')
 def sayHi():
     return {'hello':'FastAPI'}
+
 # path & Query parameters: -----------------------------
+
 @app.get('/users/{userId}')
 def getUser(userId: int):
     return {'user id': userId}
+
 # ------------------------------------------------------
+
 @app.get('/users/')
 def getUserData(userId: int, userName: str):
     return {'user id': userId, 'user name': userName}
 # follow the link bellow to try this out
 # http://127.0.0.1:8000/users/?userId=1&userName=Hassan
+
+# ------------------------------------------------------
+
+# optional parameter -----------------------------------
+# first we should import 'optional' from 'typing' ------
+@app.get('/optionalText/')
+async def optionalText(optionalText: Optional[str]):
+    return {'optional text': optionalText}
+# ctrl+click to use link:
+# link 1(empty text): http://127.0.0.1:8000/optionalText/?optionalText=
+# link 2(with text) : http://127.0.0.1:8000/optionalText/?optionalText=Elazzouzi
 # ------------------------------------------------------
