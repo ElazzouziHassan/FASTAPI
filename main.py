ddfd65d55866c891bd4cdbe46e95b6a845f00650
wizardy <wizardhassan99@gmail.com>
2023-08-02 13:15:17 +0100

    path & query parameters

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ app = FastAPI()
 @app.get('/')
 def sayHi():
     return {'hello':'FastAPI'}
+# path & Query parameters: -----------------------------
+@app.get('/users/{userId}')
+def getUser(userId: int):
+    return {'user id': userId}
+# ------------------------------------------------------
+@app.get('/users/')
+def getUserData(userId: int, userName: str):
+    return {'user id': userId, 'user name': userName}
+# follow the link bellow to try this out
+# http://127.0.0.1:8000/users/?userId=1&userName=Hassan
+# ------------------------------------------------------
